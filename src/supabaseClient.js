7ba8dfb027863f2ccc686372924e46091462913a
Haidar Gerardo Sabag Durán <haidar@iMac-de-Haidar.local>
2026-03-09 23:12:23 -0600

    fix: use correct anon JWT key for Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://lufymhcymrwavfrjjuwu.supabase.co';
-const supabaseKey = 'sb_publishable_o7e0eEKSQF3SlFNiEucefA_5V5yp-pL';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1ZnltaGN5bXJ3YXZmcmpqdXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzE1MjIsImV4cCI6MjA4ODY0NzUyMn0.DR0e7WAD6LmWIZSnRceLiwB50501Rws4u3Z0OH6IqpQ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
